bb237c73c1b34c37af7642e84a06768b541e67b3
FAL <falworks.contact@gmail.com>
2020-12-15 00:32:44 +0900

    improve comments

--- src/shared.ts
+++ src/shared.ts
@@ -1,29 +1,29 @@
 import p5 from "p5";
 import { ScaledCanvas } from "./canvas";
 
-/** The shared `p5` instance. */
+/** `p5` instance shared within the entire sketch. */
 export let p: p5;
 
 export const setP5Instance = (instance: p5): void => {
   p = instance;
 };
 
-/** The shared `ScaledCanvas` instance. */
+/** `ScaledCanvas` instance shared within the entire sketch. */
 export let canvas: ScaledCanvas;
 
 export const setCanvas = (instance: ScaledCanvas): void => {
   canvas = instance;
 };
 
-/** The HTMLElement that contains the canvas. */
+/** The HTML element that contains the canvas (default: `document.body`). */
 export let rootElement: HTMLElement = document.body;
 
-/** Sets `element` as the root so that it will contain the canvas. */
+/** Sets `element` as the root so that it contains the canvas. */
 export const setRootElement = (element: HTMLElement): void => {
   rootElement = element;
 };
 
-/** Sets the element of `id` as the root so that it will contain the canvas. */
+/** Sets the element of `id` as the root so that it contains the canvas. */
 export const setRootElementID = (id: string): void => {
   const element = document.getElementById(id);
   if (element) rootElement = element;
