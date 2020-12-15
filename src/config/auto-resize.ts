54f48679b5cef1a4ca6df8c6f263ef22aefe9cf0
FAL <falworks.contact@gmail.com>
2020-12-15 22:05:19 +0900

    bug fix: enable autoResizeCanvas() after start()

--- src/config/auto-resize.ts
+++ src/config/auto-resize.ts
@@ -1,10 +1,11 @@
 import p5 from "p5";
+import { p } from "../shared";
 import { createCanvasSizeWatcher, resizeCanvas } from "../canvas";
 import { init } from "../sketch";
 
 /**
  * Enables auto-resize so that the canvas is resized automatically
- * if the required size has been changed.
+ * whenever the required size has been changed.
  */
 export const autoResizeCanvas = (
   onCanvasResized?: () => void,
@@ -21,7 +22,6 @@ export const autoResizeCanvas = (
     };
   };
 
-  init.onCompleteAssignP5.push(apply);
+  if (p) apply(p);
+  else init.onCompleteAssignP5.push(apply);
 };
-
-export { setRootElement, setRootElementID } from "../shared";
