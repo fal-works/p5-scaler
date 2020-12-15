5288e4aa3ebdc6697857b7c5973c5a3e494d7e4b
FAL <falworks.contact@gmail.com>
2020-12-15 21:54:01 +0900

    exlude RendererType from exports

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ export * from "./entry";
 
 export { P5Methods } from "./p5-methods";
 export { ScaledCanvas } from "./canvas";
-export { RendererType, init } from "./sketch";
+export { init } from "./sketch";
 export { p, canvas, rootElement } from "./shared";
 export { drawOnCanvas } from "./draw";
 export * as config from "./config";
