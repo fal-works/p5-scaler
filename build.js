a922954b3c99183b0bb4e48c320e0942beba0fc0
FAL <falworks.contact@gmail.com>
2020-12-24 05:42:33 +0900

    update bundle-helper

--- build.js
+++ build.js
@@ -1,36 +1,41 @@
 const sltr = require("@fal-works/s-l-t-r");
-const builder = require("@fal-works/bundle-helper/lib/build/for-browsers");
+const builder = require("@fal-works/bundle-helper/lib/build/browser-module");
 
 // ----------------------------------------------------------------------------
 
 const version = "0.2.0";
 const year = "2020";
 
-const distName = "p5-scaler";
+const moduleName = "p5-scaler";
 
-const banner = `/**
- * ${distName}
- * --------------------------------------------------------------------------
- * A small library for scaling p5.js sketches.
- * https://fal-works.github.io/${distName}/
- * @copyright ${year} FAL
- * @license MIT
- * @version ${version}
- */
+const bannerContent = `
+${moduleName}
+A small module for scaling p5.js sketches.
+@see https://fal-works.github.io/${moduleName}
+@copyright ${year} FAL
+@license MIT
+@version ${version}
 `;
 
 const iifeVarName = "p5s";
 
-const build = builder.command({
-  distName,
-  banner,
+const config = {
+  bundleDistName: moduleName,
+  bannerContent,
+  iifeVarName,
   distDir: "lib",
   typesDir: "types",
   tsOutDir: "out",
-  iifeVarName,
   srcEntryFileName: "index.ts",
-  tscArgs: ["--skipLibCheck"],
-});
+};
+
+const devConfig = {
+  ...config,
+  format: false,
+};
+
+const dev = builder.command(devConfig);
+const build = builder.command(config);
 
 // ----------------------------------------------------------------------------
 
@@ -41,5 +46,5 @@ const { cmd, seq, builtin, tools } = sltr;
 const { cleandir } = builtin;
 const docs = seq(cleandir("docs"), cmd("typedoc"));
 
-const router = tools.createRouter({ build, docs }, build);
+const router = tools.createRouter({ build, dev, docs }, dev);
 router.run(process.argv[2]);
